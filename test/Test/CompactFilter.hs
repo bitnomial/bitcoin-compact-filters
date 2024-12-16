5c8323d1879dbbe381acb1e16860b20db34b51d1
Timothy Miller <timothy.miller@bitnomial.com>
2024-12-16 13:20:07 -0500

    Bump version constraints

--- test/Test/CompactFilter.hs
+++ test/Test/CompactFilter.hs
@@ -2,16 +2,16 @@ module Test.CompactFilter (
     genBlockFilter,
 ) where
 
+import Bitcoin.CompactFilter (BlockFilter, encodeFilter)
 import Control.Monad (replicateM)
-import Haskoin.Constants (btc)
+import Haskoin.Crypto (Ctx)
+import Haskoin.Network.Constants (btc)
 import Haskoin.Util.Arbitrary.Block (arbitraryBlock)
 import Haskoin.Util.Arbitrary.Util (arbitraryBS1)
 import Test.Tasty.QuickCheck (Gen, choose)
 
-import Bitcoin.CompactFilter (BlockFilter, encodeFilter)
-
-genBlockFilter :: Gen BlockFilter
-genBlockFilter = encodeFilter <$> scripts <*> arbitraryBlock btc
+genBlockFilter :: Ctx -> Gen BlockFilter
+genBlockFilter ctx = encodeFilter <$> scripts <*> arbitraryBlock btc ctx
   where
     scripts = do
         n <- choose (1, 100)
